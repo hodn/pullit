1b03faf26323dd546adf406c27d5a550c1777937
Doan, Hoang <doanhoan@fbmi.cvut.cz>
2019-04-10 09:52:46 +0200

    COM Port - serial read

---
+++ src/electron-starter.js
@@ -0,0 +1,81 @@
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const Delimiter = require('@serialport/parser-delimiter')
+
+const SerialPort = require('serialport')
+const port = new SerialPort('com5', {
+  baudRate: 9600
+})
+const parser = port.pipe(new Delimiter({ delimiter: '\n' }))
+  // Switches the port into "flowing mode"
+  parser.on('data', function (data) {
+        console.log('Data:', data.toString("utf-8"))
+    
+  })
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:3000');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
+ipc.on("componentDidMount", function(event) {
+
+  const options = {
+    type: 'info',
+    title: 'Component mounted',
+    message: 'It was mounted 5 seconds ago',
+    detail: 'React Component',
+  };
+  dialog.showMessageBox(options);
+})
+
