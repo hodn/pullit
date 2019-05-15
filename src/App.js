457be96a0193915726a1b554bb449b31f9f676a4
Doan, Hoang <doanhoan@fbmi.cvut.cz>
2019-05-15 12:48:52 +0200

    COM selection, CSV export

--- src/App.js
+++ src/App.js
@@ -18,8 +18,6 @@ class RTLineGraph extends React.Component{
   }
 
   componentDidMount() {
-    //Message for Electron
-    ipcRenderer.send('clear-to-send') 
     //Listener on parsed data from Electron
     ipcRenderer.on('data-parsed', (event, arg) => {
       
@@ -30,7 +28,7 @@ class RTLineGraph extends React.Component{
 
       const aggValue = arg[1][this.props.channel-1]
 
-      this.setState({events: newEvents, value: aggValue.toFixed(2)})
+      this.setState({events: newEvents, value: aggValue})
       
     })
 
@@ -46,8 +44,9 @@ class RTLineGraph extends React.Component{
     
         <div>
           <h2>{this.props.name}</h2>
-          <p>{this.state.value} {this.props.unit}</p>
+          <h1>{this.state.value} {this.props.unit}</h1>
           <SmoothieComponent
+              responsive
               width="553" 
               height="195"
               interpolation='linear'
@@ -74,43 +73,95 @@ class RTLineGraph extends React.Component{
     
 }
 
-class StateBar extends React.Component{
+class RecordButton extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      streaming: false
+      recordingON: false
     }
-
+    this.changeRecording = this.changeRecording.bind(this)
   }
 
   componentDidMount() {
-    ipcRenderer.on('data-parsed', (event, arg) => {
-      this.setState({streaming: true})
-    })
+
   }
 
   componentWillUnmount(){
     
 }
-  
+  changeRecording() {
+    this.setState(state => ({
+      recordingON: !state.recordingON
+    }));
+
+    ipcRenderer.send('recording', !this.state.recordingON)
+  }
+
 //What the actual component renders
   render(){    
+    
+    if (this.state.recordingON === false){ 
+      return(
+    
+      <button onClick={this.changeRecording} >Recording START</button>
+    
+  )}
+    
+    else {
+      return(
+    
+        <button onClick={this.changeRecording} style={{color:'red'}}>Recording STOP</button>
       
-   if(this.state.streaming === true) {
+    )}
+  }
+    
+}
 
-      return(
-    
-        <p></p>
-      )
-    
-    
-    }else{
-
-      return(
-    
-        <p>NO</p>
-      )
+class PortConnect extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      portList: []
     }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+   ipcRenderer.send("list-ports")
+   ipcRenderer.on('ports-listed', (event, arg) => {
+
+    const list = this.state.portList
+    list.push(arg)
+
+    this.setState({
+      portList: list,
+    })
+    
+   })
+  }
+
+  componentWillUnmount(){
+    
+}
+
+handleChange(event) {
+  console.log(event.target.value)
+  ipcRenderer.send('clear-to-send', event.target.value) 
+  }
+  
+
+//What the actual component renders
+  render(){    
+
+    return(
+      <select onChange={this.handleChange} value={this.state.selectedPort}>
+      <option value="select">Select PORT</option>
+        {this.state.portList.map((item, index) => (
+        <option key={index} value={item}>{item}</option>
+      ))}
+      </select>
+    )
+  
   }
     
 }
@@ -122,12 +173,12 @@ class StateBar extends React.Component{
     
     return (
       <div className="App">
-       <StateBar/>
+      <RecordButton/>
+      <PortConnect/>
       <RTLineGraph name="Channel 1" channel={1} unit="mV"/>
       <RTLineGraph name="Channel 2" channel={2} unit="mV"/>
       <RTLineGraph name="Channel 3" channel={3} unit="mV"/>
       <RTLineGraph name="Channel 4" channel={4}/>
-      
       </div>
     );
   }
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,3 +1,5 @@
+const { Parser } = require('json2csv');
+const fs = require('fs');
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
@@ -37,7 +39,13 @@ app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
+        
+        if(recordingON === true){
+            csv = saveRecord(csv)
+        }
+
         app.quit()
+        
     }
 });
 
@@ -55,33 +63,50 @@ app.on('activate', function () {
 
 //////////////////////////////////////// Parsing data from COM port ////////////////////////////////////////
 
-const path = require('path');
-const url = require('url');
-const Delimiter = require('@serialport/parser-delimiter');
-
+const Delimiter = require('@serialport/parser-delimiter')
+const ipcMain = electron.ipcMain
 
 //Establishing connection with COM PORT
 const SerialPort = require('serialport')
-SerialPort.list().then(
-    ports => ports.forEach(console.log),
-    err => console.error(err)
-  )
-const port = new SerialPort('com6', {
-  baudRate: 115200
+
+ipcMain.on('list-ports', (event, arg) => {
+    let availablePorts = []
+
+    SerialPort.list().then(
+    ports => ports.forEach(function(port) {
+    event.sender.send('ports-listed', port.comName)
+  }),
+    err => console.error(err),
+)
 })
-const ipcMain = electron.ipcMain
+   
+let csv = []
+let recordingON = null
+const refreshRate = 10
+
+ipcMain.on('recording', (event, arg) => {
+  recordingON = arg
+  if (arg === false){
+      csv = saveRecord(csv)
+  }
+  })
 
 //Listener on React component mounting
 ipcMain.on('clear-to-send', (event, arg) => {
-    const parser = port.pipe(new Delimiter({ delimiter: [0x00] }))
 
+    let port = new SerialPort(arg, {
+        baudRate: 115200
+      })
+   
+    const parser = port.pipe(new Delimiter({ delimiter: [0x00] }))
+    
     const ch1Buffer = []
     const ch2Buffer = []
     const ch3Buffer = []
     const ch4Buffer = []
     let aggSet = []
     let dataSet = []
-    
+
     // Switches the port into "flowing mode"
         parser.on('data', function(data) {
             
@@ -92,19 +117,33 @@ ipcMain.on('clear-to-send', (event, arg) => {
             ch3Buffer.push(channelData[2])
             ch4Buffer.push(channelData[3])
 
-            if (ch4Buffer.length > 10) {
-                
-                aggSet = [aggregator(ch1Buffer), aggregator(ch2Buffer), aggregator(ch3Buffer)]
-                ch1Buffer.splice(0, 10)
-                ch2Buffer.splice(0, 10)
-                ch3Buffer.splice(0, 10)
-                ch4Buffer.splice(0, 10)
+            if (ch4Buffer.length > refreshRate) {
+                const ch1 = aggregator(ch1Buffer)
+                const ch2 = aggregator(ch2Buffer)
+                const ch3 = aggregator(ch3Buffer)
+                aggSet = [ch1, ch2, ch3]
+
+                ch1Buffer.splice(0, refreshRate)
+                ch2Buffer.splice(0, refreshRate)
+                ch3Buffer.splice(0, refreshRate)
+                ch4Buffer.splice(0, refreshRate)
+
+                if(recordingON === true){
+                    const csvSet = {
+                        'Time': new Date(), 
+                        'ch1': ch1, 
+                        'ch2': ch2, 
+                        'ch3': ch3
+                    }
+                    csv.push(csvSet)
+                }
             }
             
             dataSet = [Date.now(), channelData[0], channelData[1], channelData[2], channelData[3]]
             event.sender.send('data-parsed', [dataSet, aggSet])
             
   });
+
   })
 
  
@@ -140,7 +179,29 @@ function aggregator(bufferArray){
     for(var i = 0; i < bufferArray.length; i++) {
     total += bufferArray[i];
 }
-    let avg = total / 10;
-    return avg
+    let avg = total / bufferArray.length;
+    return avg.toFixed(2)
 
+}
+
+function saveRecord(record){
+    const d = new Date();
+    const year = d.getFullYear().toString()
+    const month = d.getMonth().toString()
+    const day = d.getDate().toString()
+    const hour = d.getHours().toString()
+    const minute = d.getMinutes().toString()
+    const second = d.getSeconds().toString()
+    
+    const fileName = year + month + day + '_' + hour + minute + 'S' + second + '.csv'
+    
+    const fields = ['Time', 'ch1', 'ch2', 'ch3']
+        const json2csvParser = new Parser({ fields })
+        const csvOut = json2csvParser.parse(record)
+    
+        fs.writeFile(fileName, csvOut, function (err) {
+        if (err) throw err;
+
+        return []
+});
 }
